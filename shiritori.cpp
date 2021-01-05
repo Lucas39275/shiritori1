027e71c5b9c45d25c84927e1011cabb7ca72b6fa
Lucas39275 <rkawa0707@gmail.com>
2021-01-06 00:02:07 +0900

    first

---
+++ shiritori.cpp
@@ -0,0 +1,44 @@
+  #define _GLIBCXX_DEBUG
+    #include <iostream>
+    #include <iomanip>
+    #include <cctype>
+    #include <string>
+    #include <cmath>
+    #include <algorithm>
+    #include <vector>
+    #include<set>
+    using namespace std;
+    #define rep(i,n) for(int i = 0; i < n; i++)
+     
+     
+     
+    int main() {
+     
+    string s;
+    char a;
+    int b;
+    int n;
+    cin>>n;
+    set<string>siritori; //文字列の集合を用意する
+    for(int i=0;i<n;i++){
+        cin>>s;
+        b=s.size()-1;
+        if(i==0){
+             siritori.insert(s);// 文字列の挿入する
+             a=s[b]; // 文字列の一番最後の文字を代入
+             
+        
+        }
+        // 文字列の集合が入っていない事と、しりとりが成り立っているならば条件が成立する。
+        else if(siritori.count(s)==0 && s[0]==a){
+        siritori.insert(s);
+        a=s[b];
+        }
+        else{
+            cout<<"No"<<endl;
+            return 0;
+        }
+    }
+    cout<<"Yes"<<endl;
+    return 0;
+    }
